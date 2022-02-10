5aee59005b9fe786d21dd6579a9c0cf1d3587744
szcloud13 <99399764+szcloud13@users.noreply.github.com>
2022-02-10 22:33:54 +1100

    Update run.py

--- backend/run.py
+++ backend/run.py
@@ -8,7 +8,7 @@ if not os.path.exists(database_dir):
 if not os.path.exists(database_file):
     print('Creating', database_file)
     import ssl, urllib.request
-    with urllib.request.urlopen('https://www.cse.unsw.edu.au/~cs2041/18s2/activities/instacram/test.sqlite3', context = ssl.SSLContext(ssl.PROTOCOL_TLSv1)) as response:
+    with urllib.request.urlopen('../instacram/test.sqlite3', context = ssl.SSLContext(ssl.PROTOCOL_TLSv1)) as response:
         db = response.read()
     with open(database_file, "wb") as f:
         f.write(db)
@@ -25,5 +25,3 @@ except ImportError as e:
         print('The backend requires Python 3.6 or later - you appear to be using Python {}.{}'.format(*sys.version_info), file=sys.stderr)
     else:
         print('A module required by the backend is missing.', file=sys.stderr)
-        print('See the instructions in backend/README.md for installing the required modules.', file=sys.stderr)    
-        print('Ask in the forum if you can not fix this problem.', file=sys.stderr) 
